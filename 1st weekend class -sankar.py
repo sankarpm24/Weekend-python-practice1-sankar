77f653e7b282a4f06484985884df6be8a977b8e3
sankarpm24 <53351504+sankarpm24@users.noreply.github.com>
2019-07-27 13:26:12 +0530

    Add files via upload

---
+++ 1st weekend class -sankar.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+print sys
+
+
+# In[2]:
+
+
+import sys
+print(sys.version)
+
+
+# In[3]:
+
+
+import sys
+print(sys)
+
+
+# In[4]:
+
+
+import sys
+print sys.version()
+
+
+# In[5]:
+
+
+import sys
+print(sys.version)
+
+
+# In[6]:
+
+
+#introduction to variables
+x=10
+y=5
+x+y
+
+
+# In[7]:
+
+
+x=6
+y=2
+print(x/y)
+
+
+# In[8]:
+
+
+x=6
+y=8
+
+
+# In[9]:
+
+
+x=6
+y=8
+x+y
+y=8
+
+
+# In[10]:
+
+
+x=6
+y=8
+x+y
+
+
+# In[11]:
+
+
+message="Hello Python world"
+print(message)
+
+
+# In[12]:
+
+
+first name ="sankar"
+last name ="P"
+print(first name)
+
+
+# In[13]:
+
+
+first_name ="sankar"
+last_name ="P"
+print(first_name)
+
+
+# In[14]:
+
+
+first_name ="sankar"
+last_name ="P"
+print(first_name,last_name)
+
+
+# In[15]:
+
+
+first_name ="sankar"
+last_name ="P"
+print(first_name last_name)
+
+
+# In[16]:
+
+
+first_name ='sankar'
+last_name ='P'
+print(first_name,last_name)
+
+
+# In[17]:
+
+
+name="sankar p"
+print(name.title)
+
+
+# In[18]:
+
+
+name="sankar p"
+print(name.title())
+
+
+# In[19]:
+
+
+name="sankar p"
+print(name.lower())
+print(name.upper())
+
+
+# In[20]:
+
+
+first_name ='sankar'
+last_name ='P'
+full_name=f"{first_name}{last_name}
+
+
+# In[21]:
+
+
+first_name ='sankar'
+last_name ='P'
+full_name=f"{first_name}{last_name}""
+
+
+# In[22]:
+
+
+first_name ='sankar'
+last_name ='P'
+full_name=f"{first_name}{last_name}"
+
+
+# In[23]:
+
+
+first_name ='sankar'
+last_name ='P'
+full_name=f"{first_name}{last_name}"
+print(full_name)
+
+
+# In[24]:
+
+
+first_name ='sankar'
+last_name ='P'
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[25]:
+
+
+first_name ='sankar'
+last_name ='P'
+f"{first_name} {last_name}"
+
+
+# In[26]:
+
+
+print("python")
+print("python ")
+print("\tpython")
+
+
+# In[27]:
+
+
+first_name ='sankar'
+first_name ="sankar"
+first_name =""sankar"""
+
+
+# In[28]:
+
+
+first_name1 ='sankar'
+first_name2 ="sankar"
+first_name3 =""sankar""
+print first_name1
+print first_name2
+print first_name3 
+
+
+# In[29]:
+
+
+first_name1 ='sankar'
+first_name2 ="sankar"
+print first_name1
+print first_name2
+
+
+# In[30]:
+
+
+first_name1 ='sankar'
+first_name2 ="sankar"
+print (first_name1)
+print (first_name2)
+
+
+# In[31]:
+
+
+print("python\nC\nC++")
+
+
+# In[32]:
+
+
+print("python\n C\n C++")
+
+
+# In[33]:
+
+
+fav_lang="python "
+print(fav_lang.strip())
+
+
+# In[ ]:
+
+
+
+
